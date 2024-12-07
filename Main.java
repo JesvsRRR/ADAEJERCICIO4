2226ef3eb6c0ec7a9fdf52facd635a383fce5e98
JesvsRRR <jesusronaldrosales05@hotmail.com>
2024-12-07 11:07:09 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class Main {
 
     public static void transpuesta(int n,int m){
@@ -16,18 +15,22 @@ public class Main {
         }
     }
     
-    System.out.println("TRANSPUESTA:");
+    System.out.println("MATRIZ ORIGINAL:");
+     for(int i=0; i < n; i++){
+        for(int j=0; j < m;j++){
+         System.out.print("["+Matriz[i][j]+"]"); 
+        }
+    System.out.println("");
+    }
     
+    System.out.println("MATRIZ TRANSPUESTA:");
     for(int i=0; i < m; i++){
         for(int j=0; j < n;j++){
         Transpuesta[i][j] = Matriz[j][i];
         System.out.print("["+Transpuesta[i][j]+"]"); 
         }
     System.out.println("");    
-    }
-    
-    
-    
+    }   
     }
 
     public static void main(String[] args) {
@@ -48,3 +51,4 @@ public class Main {
     }
     
 }
+
